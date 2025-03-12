5a86f50bb4b8f490ed13bc5c4a4af263828ae926
DELL <salmabendriss96@gmail.com>
2025-03-13 00:17:19 +0800

    Extract city information from airports data

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ airports_df = spark.read.csv("/flight_data/airports", header=True, inferSchema=T
 itineraries_df = spark.read.csv("/flight_data/itineraries", header=True, inferSchema=True)
 # Load countries data
 countries_df = spark.read.csv("/flight_data/countries", header=True, inferSchema=True)
+
+# Extract city information from airports data
+cities_df = airports_df.select(
+    col("municipality").alias("city_name"),
+    col("iata_code").alias("city_code"),
+    col("latitude_deg").alias("latitude"),
+    col("longitude_deg").alias("longitude"),
+    col("iso_country").alias("country_code")
+).distinct()
